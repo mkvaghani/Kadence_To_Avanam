a2e0b2dd5e56dc751c2cdada500ae732cac57132
mkvaghani <mkvaghani@gmail.com>
2024-01-16 09:44:38 +0530

    First Commit from Avanam Generator - Kadence1.1.51
    Step 1: Login Putty
    Step 2:
    Login admin and update theme
    https://avanam.org/generator/wp-admin/themes.php
    
    Step 3:
    Upload Kadence Pro Plugin
    https://kernl.us/api/v1/updates/5eee71ef08f6d93d2b905870/
    https://kernl.us/api/v1/updates/5eee71ef08f6d93d2b905870/download/
    
    Step 4: Permission
    
    sudo chown -R $USER:www-data /var/www/html/avanam.org/public_html && \
    sudo find /var/www/html/avanam.org/public_html -type d -exec chmod 770 {} \;  && \
    sudo find /var/www/html/avanam.org/public_html -type f -exec chmod 660 {} \;
    
    Step 4: Generate Prototype from Kadence
    
    Then run
    https://avanam.org/generator/cron/theme-generator.php
    
    It will create prototype  "themename"
    /var/www/html/avanam.org/public_html/generator/wp-content/themes/basetheme/plugins/prototype / themename
    
    Step 4: Generate Plugin Prototype from Kadence Pro
    
    https://avanam.org/generator/cron/theme-generator.php?plugins=1
    
    It will create prototype  "plugins"
    /var/www/html/avanam.org/public_html/generator/wp-content/themes/basetheme/plugins/prototype / plugins
    
    Now Generate "Avanam" from
    https://avanam.org/generator/
    Enter name : Avanam
    Dropdown: Parent Theme
    
    https://avanam.org/generator/
    Enter name : Avanam
    Dropdown: Pro Plugin

---
+++ inc/dashboard/react/src/customizer.js
@@ -0,0 +1,76 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+const { Fragment } = wp.element;
+import map from 'lodash/map';
+const { withFilters, TabPanel, Panel, PanelBody, PanelRow, Button } = wp.components;
+
+export const CustomizerLinks = () => {
+	const headerLinks = [
+		{
+			title: __( 'Global Colors', 'avanam' ),
+			description: __( 'Setup the base color scheme for your site.', 'avanam' ),
+			focus: 'base_customizer_general_colors',
+			type: 'section',
+			setting: false
+		},
+		{
+			title: __( 'Branding', 'avanam' ),
+			description: __( 'Upload your logo and favicon.', 'avanam' ),
+			focus: 'title_tagline',
+			type: 'section',
+			setting: false
+		},
+		{
+			title: __( 'Typography', 'avanam' ),
+			description: __( 'Choose the perfect font family, style and sizes.', 'avanam' ),
+			focus: 'base_customizer_general_typography',
+			type: 'section',
+			setting: false
+		},
+		{
+			title: __( 'Header Layout', 'avanam' ),
+			description: __( 'Add elements and arrange them how you want.', 'avanam' ),
+			focus: 'base_customizer_header',
+			type: 'panel',
+			setting: false
+		},
+		{
+			title: __( 'Page Layout', 'avanam' ),
+			description: __( 'Define your sites general page look and feel for page title, and content style.', 'avanam' ),
+			focus: 'base_customizer_page_layout',
+			type: 'section',
+			setting: false
+		},
+		{
+			title: __( 'Footer Layout', 'avanam' ),
+			description: __( 'Customize the columns and place widget areas in unlimited configurations', 'avanam' ),
+			focus: 'base_customizer_footer_layout',
+			type: 'section',
+			setting: false
+		},
+	];
+	return (
+		<Fragment>
+			<h2 className="section-header">{ __( 'Customize Your Site', 'avanam' ) }</h2>
+			<div className="two-col-grid">
+				{ map( headerLinks, ( link ) => {
+					return (
+						<div className="link-item">
+							<h4>{ link.title }</h4>
+							<p>{ link.description }</p>
+							<div className="link-item-foot">
+								<a href={ `${baseDashboardParams.adminURL}customize.php?autofocus%5B${ link.type }%5D=${ link.focus }` }>
+									{ __( 'Customize', 'avanam') }
+								</a>
+							</div>
+						</div>
+					);
+				} ) }
+			</div>
+		</Fragment>
+	);
+};
+
+export default withFilters( 'base_theme_customizer' )( CustomizerLinks );
---
+++ inc/dashboard/react/src/sidebar.js
@@ -0,0 +1,32 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+const { Fragment } = wp.element;
+const { withFilters, TabPanel, Panel, PanelBody, PanelRow, Button } = wp.components;
+export const Sidebar = () => {
+	return (
+		<Fragment>
+			<Panel className="community-section sidebar-section">
+				<PanelBody
+					opened={ true }
+				>
+					<h2>{ __( 'Web Creators Community', 'avanam' ) }</h2>
+					<p>{ __( 'Join our community of fellow base users creating effective websites! Share your site, ask a question and help others.', 'avanam' ) }</p>
+					<a href="https://www.facebook.com/groups/" target="_blank" class="sidebar-link">{ __( 'Join our Facebook Group', 'avanam' ) }</a>
+				</PanelBody>
+			</Panel>
+			<Panel className="support-section sidebar-section">
+				<PanelBody
+					opened={ true }
+				>
+					<h2>{ __( 'Support', 'avanam' ) }</h2>
+					<p>{ __( 'Have a question, we are happy to help! Get in touch with our support team.', 'avanam' ) }</p>
+					<a href="https://avanam.org/wordpress#support" target="_blank" class="sidebar-link">{ __( 'Submit a Ticket', 'avanam' ) }</a>
+				</PanelBody>
+			</Panel>
+		</Fragment>
+	);
+};
+
+export default withFilters( 'base_theme_sidebar' )( Sidebar );
